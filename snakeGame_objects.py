28f5b2d1adbb09471d495758618711252edf5119
WilliamTheCheeseWizard <williamdeweese.wd@gmail.com>
2026-04-08 13:28:35 -0600

    Created SnakeBody Class and Added Color to object class
    Updated Object class to include color attribute and modified string representation. Added Snakebody class with incomplete implementation.

--- snakeGame_objects.py
+++ snakeGame_objects.py
@@ -21,16 +21,14 @@ class Food(Particle):
 class Object:
     """Base class for all objects on grid"""
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
-        
-        self.r = 120
-        self.g = 120
-        self.b = 120
+
+        self.color = color
 
     def __str__(self):
-        return (f"coordinate: ({self.x}, {self.y}), RGB: {self.r,self.g,self.b}")
+        return (f"coordinate: ({self.x}, {self.y}), RGB: {self.color}")
 
     def __repr__(self):
         return self.__str__()
@@ -64,3 +62,9 @@ class Food(Object):
     def draw( self ) :
         dudraw.set_pen_color( Minnow.MINNOW )
         dudraw.filled_rectangle( self.x+0.5, self.y+0.5, 0.45, 0.45 )
+
+class Snakebody(Object):
+
+    dudraw.color
+
+    def __init__(self, x, y)
